fa94e742c625659d146758fc7aaad1305546ee4a
Elena <olimpiada100@yandex.ru>
2020-12-25 22:05:35 +0300

    fix linebreak-style in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
         sourceType: "module",
     },
     rules: {
-        "linebreak-style": ["error", "windows"],
+        "linebreak-style": 0,
         "import/prefer-default-export": "off",
         "no-plusplus": "off",
         "comma-dangle": ["error", "never"],
